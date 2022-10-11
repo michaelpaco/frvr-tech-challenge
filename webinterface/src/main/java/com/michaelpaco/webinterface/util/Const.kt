9169b9ae9061bf05cc46e7ae85e9df6df9642d1f
nb27419 <Michaelmike2512>
2022-10-12 00:48:25 +0100

    feat: add Consts object

---
+++ webinterface/src/main/java/com/michaelpaco/webinterface/util/Const.kt
@@ -0,0 +1,8 @@
+package com.michaelpaco.webinterface.util
+
+object Const {
+    const val DB_NAME = "LocalNotification.db"
+    const val NATIVE_LOGS_PARAM = "native_logs"
+    const val LOCAL_NOTIFICATIONS_PARAM = "local_notifications"
+    const val LIFECYCLE_EVENTS_PARAM = "lifecycle_events"
+}
