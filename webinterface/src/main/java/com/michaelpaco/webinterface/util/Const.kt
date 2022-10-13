202a80b9899e03ec918ba565d6aa70000fc85a43
nb27419 <Michaelmike2512>
2022-10-13 18:08:05 +0100

    feat: update consts and strings

--- webinterface/src/main/java/com/michaelpaco/webinterface/util/Const.kt
+++ webinterface/src/main/java/com/michaelpaco/webinterface/util/Const.kt
@@ -1,8 +1,12 @@
 package com.michaelpaco.webinterface.util
 
 object Const {
-    const val DB_NAME = "LocalNotification.db"
+    const val NOTIFICATION_DATABASE_NAME = "LocalNotification.db"
+    const val LIFECYCLE_EVENT_DATABASE_NAME = "LifecycleEvent.db"
     const val NATIVE_LOGS_PARAM = "native_logs"
     const val LOCAL_NOTIFICATIONS_PARAM = "local_notifications"
     const val LIFECYCLE_EVENTS_PARAM = "lifecycle_events"
+    const val LOCAL_BASE_URL = "file:///android_asset/webview/index.html"
+    const val JAVASCRIPT_INTERFACE_NAME = "Android"
+    const val WEBVIEW_MESSAGE_TAG = "WebViewMessage"
 }
